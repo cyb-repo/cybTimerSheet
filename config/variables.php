8af5627b3a710e1e1fc5b12cbcf69f9cdaa25d53
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-04-16 21:45:21 +0000

    project setup & user auth

---
+++ config/variables.php
@@ -0,0 +1,29 @@
+<?php
+  // Variables
+  return [
+    "creatorName" => "CybTime",
+    "creatorUrl" => "boystack",
+    "templateName" => " ",
+    "templateSuffix" => "CybTime",
+    "templateVersion" => "1.0.0",
+    "templateFree" => false,
+    "templateDescription" => "Start your journey with CybTime, a powerful and creative admin template.",
+    "templateKeyword" => "dashboard, boystack",
+    "licenseUrl" => "http://boystack.com/",
+    "livePreview" => "http://boystack.com/",
+    "productPage" => "http://boystack.com/",
+    "support" => "http://boystack.com/",
+    "moreThemes" => "http://boystack.com/",
+    "documentation" => "http://boystack.com/",
+    "generator" => "",
+    "changelog" => "http://boystack.com/",
+    "repository" => "http://boystack.com/",
+    "gitRepo" => "abdelaliabouelhassan",
+    "gitRepoAccess" => "abdelali abouelhassan",
+    "githubFreeUrl" => "http://boystack.com/",
+    "facebookUrl" => "http://boystack.com/",
+    "twitterUrl" => "http://boystack.com/",
+    "githubUrl" => "http://boystack.com/",
+    "dribbbleUrl" => "http://boystack.com/",
+    "instagramUrl" => "http://boystack.com/"
+  ];
---
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -0,0 +1,19 @@
+<!-- Footer-->
+<footer class="content-footer footer bg-footer-theme">
+  <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }}">
+    <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+      <div>
+        © <script>
+          document.write(new Date().getFullYear())
+
+        </script>
+        , all rights reserved.
+        {{-- <a href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}" target="_blank" class="fw-semibold">{{ (!empty(config('variables.creatorName')) ? config('variables.creatorName') : '') }}</a> --}}
+      </div>
+      <div>
+      
+      </div>
+    </div>
+  </div>
+</footer>
+<!--/ Footer-->
